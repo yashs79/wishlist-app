0a064ce5f67aaa5428f0f9def8f82e08b9afd23b
Yash Sharma <yashs4783@gmail.com>
2025-07-07 04:03:42 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { handleInviteRedirect } from './utils/inviteHandler';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Check if there's an invite code in the URL query parameters
+// If there is, redirect to the join page with the code in the URL path
+// This allows handling invite links like http://localhost:3000?code=ABC123
+const redirected = handleInviteRedirect();
+
+// Only render the app if we're not redirecting
+if (!redirected) {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
